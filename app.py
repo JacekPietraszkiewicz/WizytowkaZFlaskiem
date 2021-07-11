16e7f535a90442256374ab0f91e601677d73e1cc
JacekPietraszkiewicz <85499795+JacekPietraszkiewicz@users.noreply.github.com>
2021-07-11 13:01:45 +0200

    Update app.py
    Dodano komentarze

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
+# Jacek Pietraszkiewicz 2021. Zadanie Kodilla: Wizytowka z Flaskiem
+# Program definiujący dwie funkcje (hello_o i hello_k) wywoływane z localhost /me lub /contakt
+
 from flask import Flask
 from flask import request, render_template, redirect
 
 app = Flask(__name__)
 
+# Pro
 @app.route('/me', methods = ['GET', 'POST'])
 def hello_o():
     if request.method == 'GET':
-
         return render_template("o-mnie.html")
     elif request.method == 'POST':
         f = open("formatka", "a")
         f.write(request.form["firstname"])
         f.close()
         return render_template("thanks.html", kto=request.form["firstname"])
-
-    return
 
 @app.route('/contakt', methods=['GET', 'POST'])
 def hello_k():
